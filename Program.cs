57697fd431366dee46382ba1248223a2ca999691
Adryanrk <adryan.rk04@gmail.com>
2025-09-23 12:36:29 +0200

    Ändrade från Adrian till Adryan

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@
         {
             Console.WriteLine("Hello, World!");
 
-            Console.WriteLine("Hej Adrian och Benjamin");
+            Console.WriteLine("Hej Adryan och Benjamin");
         }
     }
 }
