0e9f5bd82a52ede5a5663012cdc25904d6c24201
Benjamin <benjamin.dealmeida04@gmail.com>
2025-09-23 22:59:29 +0200

    Board code

--- Board.cs
+++ Board.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,49 @@ namespace TicTacToe
 {
     internal class Board
     {
+        public char[] Square { get; private set; }
+
+        public Board()
+        {
+            Square = new char[10];
+            for (int i = 1; i <= 9; i++)
+            {
+                Square[i] = (char)('0' + i);
+            }
+        }
+        public void _Board()
+        {
+            Console.Clear();
+            Console.WriteLine(" {0} | {1} | {2} ", Square[1], Square[2], Square[3]);
+            Console.WriteLine("---+---+---");
+            Console.WriteLine(" {0} | {1} | {2} ", Square[4], Square[5], Square[6]);
+            Console.WriteLine("---+---+---");
+            Console.WriteLine(" {0} | {1} | {2} ", Square[7], Square[8], Square[9]);
+        }
+
+        
+        public bool IfSquareEmpty(int pos)
+        {
+            return Square[pos] != 'X' && Square[pos] != 'O';    // Om en ruta är ledig
+        }
+        
+        // Måste lägga in position, Spelarens symbol fyller ruta
+        // Om man lägger in fel värde
+        
+
+        
+        
+        public bool IfBoardIsFull()
+        {
+            for (int i = 1; i <= 9; i++)
+            { 
+                if (Square[i] != 'X' && Square[i] != 'O')    // Om alla rutor är upptagna 
+                    return false;   
+            }
+        return true;    
+        }
+        
+
     }
-}
+
+}     
