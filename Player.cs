5b2f502ffba5034e3683cee556a5d2b3efd2ba61
William68040 <williamluc888@outlook.com>
2025-09-29 23:30:05 +0200

    added functions to Player class

---
+++ Player.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    /// <summary>
+    /// En enkel mänsklig spelare med namn och symbol (X eller O).
+    /// </summary>
+    class Player
+    {
+        public string Name { get; }   // spelarens namn
+        public char Symbol { get; }   // 'X' eller 'O'
+
+        public Player(string name, char symbol)
+        {
+            // Om användaren inte skrev något namn -> sätt standardnamn
+            Name = string.IsNullOrWhiteSpace(name) ? "Spelare" : name;
+            Symbol = symbol;
+        }
+    }
+}
